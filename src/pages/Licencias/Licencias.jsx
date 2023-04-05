ccdaaf3f08fdf6b2f22318850ff1df37d71fcba4
Capacitaciones3 <101343285+Capacitaciones3@users.noreply.github.com>
2023-04-05 16:59:40 -0300

    Componentes de archivo y licencias

--- src/pages/Licencias/Licencias.jsx
+++ src/pages/Licencias/Licencias.jsx
@@ -4,25 +4,35 @@ import SubirArchivo from './SubirArchivo/SubirArchivo'
 import TipodeLicencia from './TipodeLicencia/TipodeLicencia'
 import Descripcion from './Descripcion/Descripcion'
 import CardPrincipal from '../../components/Cards/CardPrincipal/CardPrincipal'
+import Usuario from '../../components/Usuario/Usuario'
+import { Divider } from '@mui/material'
 
 const Licencias = () => {
   return (
     <div className='container'>
       <div className='containercolumna'>
           <div className='usuarioLicencia'>
-        
+              <Usuario></Usuario>
           </div>
           <section className='descripcionLicencia'>
-              <SubirArchivo></SubirArchivo>
+            <div>
               <TipodeLicencia></TipodeLicencia>
               <Descripcion></Descripcion>
+            </div>
+            <div>
+              <SubirArchivo></SubirArchivo>
+            </div>
           </section>
       </div>
       <aside className='lista'>
         <CardPrincipal></CardPrincipal>
+        <Divider light />
         <CardPrincipal></CardPrincipal>
+        <Divider light />
         <CardPrincipal></CardPrincipal>
+        <Divider light />
         <CardPrincipal></CardPrincipal>
+        <Divider light />
       </aside>
     </div>
   )
--- src/pages/Licencias/SubirArchivo/SubirArchivo.jsx
+++ src/pages/Licencias/SubirArchivo/SubirArchivo.jsx
@@ -1,33 +1,31 @@
 import { PhotoCamera } from '@mui/icons-material'
-import { Button, IconButton } from '@mui/material'
+import { Box, Button, Container, IconButton, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
 const SubirArchivo = () => {
 
-  const [file, setFile] = useState();
-  const [fileDisabled, setFileDisabled] = useState(false);
+    const [file, setFile] = useState(false);
+    const [message, setMessage] = useState(true);
 
-  const fileEvent = (e) => {
-    setFile(e.target.value);
-    setFileDisabled(true);
-  };
 
-  const enableFile = () => {
-    setFileDisabled(false);
-  };
 
 
   return (
-    <div>
-      <Button variant="contained" component="label">
-        Subir Archivo
-        <input hidden accept="image/*" multiple type="file"/>
-      </Button>
-      <IconButton color="primary" aria-label="upload picture" component="label">
-        <input hidden accept="image/*" type="file" />
-        <PhotoCamera />
-      </IconButton>
-  </div>
+      <Box sx={{ width: '100%', display:'flex', flexDirection: 'column', alignItems:'center'}}>
+      <Typography variant="h6" gutterBottom>
+        ARCHIVO ADJUNTO
+      </Typography>
+          <Container sx={{ width: '100%', display:'flex', alignSelf: 'center'}}>
+              <Button variant="contained" component="label" >
+                Subir Archivo
+                <input hidden accept="image/jpg, image/png" type="file"  />
+              </Button>
+              <IconButton color="primary" aria-label="upload picture" component="label">
+                <input hidden accept="image/*" type="file" />
+                <PhotoCamera />
+              </IconButton>
+          </Container>
+      </Box>
   )
 }
 
@@ -35,3 +33,15 @@ export default SubirArchivo
 
 /* onChange={fileEvent} disabled={fileDisabled}
 onClick={enableFile}*/
+
+/* const [file, setFile] = useState();
+const [fileDisabled, setFileDisabled] = useState(false);
+
+const fileEvent = (e) => {
+  setFile(e.target.value);
+  setFileDisabled(true);
+};
+
+const enableFile = () => {
+  setFileDisabled(false);
+}; */
