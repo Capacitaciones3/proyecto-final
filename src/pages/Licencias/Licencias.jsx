9c6d64403bbe5d1952f69c6c43426a14c55e5fb4
Capacitaciones3 <101343285+Capacitaciones3@users.noreply.github.com>
2023-04-06 15:20:14 -0300

    Cambios en licencias

--- src/pages/Licencias/Licencias.jsx
+++ src/pages/Licencias/Licencias.jsx
@@ -4,7 +4,8 @@ import SubirArchivo from './SubirArchivo/SubirArchivo'
 import TipodeLicencia from './TipodeLicencia/TipodeLicencia'
 import Descripcion from './Descripcion/Descripcion'
 import CardPrincipal from '../../components/Cards/CardPrincipal/CardPrincipal'
-import {BottomNavigationAction } from '@mui/material'
+import Usuario from '../../components/Usuario/Usuario'
+import { BottomNavigationAction, Divider } from '@mui/material'
 import PostAddIcon from '@mui/icons-material/PostAdd';
 
 const Licencias = () => {
@@ -12,17 +13,28 @@ const Licencias = () => {
     <div className='container'>
       <div className='containercolumna'>
           <div className='usuarioLicencia'>
-        
+              <Usuario></Usuario>
           </div>
           <section className='descripcionLicencia'>
-              <SubirArchivo></SubirArchivo>
+            <div>
               <TipodeLicencia></TipodeLicencia>
               <Descripcion></Descripcion>
-              <BottomNavigationAction label="Solicitar aprobacion" icon={<PostAddIcon />}>Solicitar aprobacion</BottomNavigationAction>
+            </div>
+            <div>
+              <SubirArchivo></SubirArchivo>
+            </div>
+            <BottomNavigationAction label="Solicitar aprobacion" icon={<PostAddIcon />}>Solicitar aprobacion</BottomNavigationAction>
           </section>
       </div>
       <aside className='lista'>
         <CardPrincipal></CardPrincipal>
+        <Divider light />
+        <CardPrincipal></CardPrincipal>
+        <Divider light />
+        <CardPrincipal></CardPrincipal>
+        <Divider light />
+        <CardPrincipal></CardPrincipal>
+        <Divider light />
       </aside>
     </div>
   )
