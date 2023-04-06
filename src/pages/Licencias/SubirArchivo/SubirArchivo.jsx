6f46d8e19b9eafb8498558ddf21969dbad1e8482
Capacitaciones3 <101343285+Capacitaciones3@users.noreply.github.com>
2023-04-06 15:17:13 -0300

    Cambios en subir archivo de licencias

--- src/pages/Licencias/SubirArchivo/SubirArchivo.jsx
+++ src/pages/Licencias/SubirArchivo/SubirArchivo.jsx
@@ -1,37 +1,28 @@
 import { PhotoCamera } from '@mui/icons-material'
-import { Button, IconButton } from '@mui/material'
+import { Box, Button, Container, IconButton, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
 const SubirArchivo = () => {
 
-  const [file, setFile] = useState();
-  const [fileDisabled, setFileDisabled] = useState(false);
-
-  const fileEvent = (e) => {
-    setFile(e.target.value);
-    setFileDisabled(true);
-  };
-
-  const enableFile = () => {
-    setFileDisabled(false);
-  };
+    const [file, setFile] = useState(false);
+    const [message, setMessage] = useState(true);
 
 
   return (
-    <div>
-      <Button variant="contained" component="label">
-        Subir Archivo
-        <input hidden accept="image/*" multiple type="file"/>
-      </Button>
-      <IconButton color="primary" aria-label="upload picture" component="label">
-        <input hidden accept="image/*" type="file" />
-        <PhotoCamera />
-      </IconButton>
-  </div>
+      <Box sx={{ width: '100%', display:'flex', flexDirection: 'column', alignItems:'center'}}>
+      <Typography variant="h6" gutterBottom>
+        ARCHIVO ADJUNTO
+      </Typography>
+          <Container sx={{ width: '100%', display:'flex', alignSelf: 'center'}}>
+              <Button variant="contained" component="label" >
+                Subir Archivo
+                <input hidden accept="image/jpg, image/png" type="file"  />
+              </Button>
+              <IconButton color="primary" aria-label="upload picture" component="label">
+                <input hidden accept="image/*" type="file" />
+                <PhotoCamera />
+              </IconButton>
+          </Container>
+      </Box>
   )
 }
-
-export default SubirArchivo
-
-/* onChange={fileEvent} disabled={fileDisabled}
-onClick={enableFile}*/
