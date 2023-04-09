122db1be89272fa52455f29b1d011ac2bd61d2ef
lagustin-adviters <capacitaciones7@adviters.com>
2023-04-09 03:03:28 -0300

    Se agregó el componente Link para redireccionar a los formularios

--- src/pages/AdminUsuarios/AdminUsuarios.jsx
+++ src/pages/AdminUsuarios/AdminUsuarios.jsx
@@ -2,6 +2,7 @@ import { Button, Typography } from "@mui/material";
 import React from "react";
 import "./AdminUsuarios.css";
 import CardAdmUsers from "../../components/Cards/CardAdmUsers/CardAdmUsers";
+import { Link } from "react-router-dom";
 
 const AdminUsuarios = () => {
   return (
@@ -26,27 +27,29 @@ const AdminUsuarios = () => {
         <CardAdmUsers id="3" avatar="" nombre="Maria Becerra" rol="Usuario" />
         <CardAdmUsers id="4" avatar="" nombre="Ricky Martin" rol="Usuario" />
       </div>
-      <Button
-        sx={{
-          position: "absolute",
-          width: "174px",
-          height: "28px",
-          left: "873px",
-          top: "32px",
-          background: "#63ECB2",
-          border: "0.5px solid #797979",
-          boxShadow: " 0px 2px 8px rgba(0, 0, 0, 0.25)",
-          borderRadius: "8px",
-          color: "#ffffff",
-          fontWeight: "600",
-          fontSize: "11px",
-          "&:hover": {
-            backgroundColor: "#63ECB2",
-          },
-        }}
-      >
-        Crear Usuario
-      </Button>
+      <Link to="/crearUsuario">
+        <Button
+          sx={{
+            position: "absolute",
+            width: "174px",
+            height: "28px",
+            left: "873px",
+            top: "32px",
+            background: "#63ECB2",
+            border: "0.5px solid #797979",
+            boxShadow: " 0px 2px 8px rgba(0, 0, 0, 0.25)",
+            borderRadius: "8px",
+            color: "#ffffff",
+            fontWeight: "600",
+            fontSize: "11px",
+            "&:hover": {
+              backgroundColor: "#63ECB2",
+            },
+          }}
+        >
+          Crear Usuario
+        </Button>
+      </Link>
     </div>
   );
 };
