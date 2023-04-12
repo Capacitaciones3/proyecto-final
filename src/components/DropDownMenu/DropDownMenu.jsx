759378c4d895013aaa19596c16fb2cd44969329c
PJoaquin-adviters <capacitaciones8@adviters.com>
2023-04-12 17:13:57 -0300

    cambios en header y agregado de calendario page

---
+++ src/components/DropDownMenu/DropDownMenu.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Menu from "@mui/material/Menu";
+import IconButton from "@mui/material/IconButton";
+import MenuItem from "@mui/material/MenuItem";
+
+const DropDownMenu = ({ name, botonIcono, listItems }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const redirect = (path, title) => {
+    window.location.href = `${path}?title=${title.toUpperCase()}`
+  }
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (e) => {
+    setAnchorEl(null);
+  };
+  return (
+    <>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        {botonIcono}
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        {listItems.map((el, index) => (
+          <MenuItem
+            key={`${name}-${index}`}
+            onClick={() => redirect(el.path, el.text)}
+            sx={{
+              display: "flex",
+              gap: "20px",
+            }}
+          >
+            {el.icono} {el.text}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+};
+
+export default DropDownMenu;
--- src/pages/Calendario/Calendario/CalendarioItem.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CalendarioItem = () => {
-  return (
-    <div>CalendarioItem</div>
-  )
-}
-
-export default CalendarioItem
