10d9428d3cdac3f99bd5f8c7043f69b1e2ee0d21
vgabriel-adviters <capacitaciones13@adviters.com>
2023-04-12 17:39:24 -0300

    Boton para la licencia en dashboard

--- src/components/LicenciaCard/LicenciaCard.jsx
+++ src/components/LicenciaCard/LicenciaCard.jsx
@@ -1,12 +1,31 @@
-
-// ESTA ES LA CARTA QUE APARECE DEBAJO DEL DASHBOARD
-
-
-import React from 'react'
+import { Avatar, Box, Button, Stack } from '@mui/material';
+import React, { useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import Licencias from '../../pages/Licencias/Licencias';
 
 const LicenciaCard = () => {
+
+  const { idLicencia } = useParams();
+  const navigate = useNavigate();
+
   return (
-    <div>LicenciaCard</div>
+    <Box>
+      <Stack direction='row' sx={{ display: 'flex', justifyContent: 'space-between', padding: '0px 40px' }}>
+        <Avatar sx={{ backgroundColor: 'orange' }}>OP</Avatar>
+        <Button variant='contained' onClick={() => navigate("/dashboard")} sx={{ 
+          backgroundColor: '#FFA1A1', 
+          minWidth: '50px', 
+          width: '50px', 
+          borderRadius: '15px', 
+          fontWeight: 'bold', 
+          fontSize: '20px',
+          "&:hover": {
+            backgroundColor: '#FF4545'
+          }
+          }}>X</Button>
+      </Stack>
+      <Licencias/>
+    </Box>
   )
 }
 
