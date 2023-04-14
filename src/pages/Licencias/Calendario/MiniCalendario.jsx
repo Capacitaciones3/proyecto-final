3cc6ce4f27d793a9aac314c59a175ef2a0f46ab5
Capacitaciones3 <101343285+Capacitaciones3@users.noreply.github.com>
2023-04-14 10:50:19 -0300

    prettier en minicalendario

--- src/pages/Licencias/Calendario/MiniCalendario.jsx
+++ src/pages/Licencias/Calendario/MiniCalendario.jsx
@@ -1,21 +1,21 @@
-import { Box, TextField, Typography } from '@mui/material';
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
-import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import dayjs from 'dayjs';
+import { Box, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import { LocalizationProvider, StaticDatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
+  "& .MuiDialogContent-root": {
     padding: theme.spacing(2),
   },
-  '& .MuiDialogActions-root': {
+  "& .MuiDialogActions-root": {
     padding: theme.spacing(1),
   },
 }));
@@ -28,34 +28,32 @@ const BootstrapDialogTitle = (props) => {
       {children}
       {onClose ? (
         <IconButton
-          aria-label="close"
+          aria-label='close'
           onClick={onClose}
           sx={{
-            position: 'absolute',
+            position: "absolute",
             right: 8,
             top: 8,
             color: (theme) => theme.palette.grey[500],
-          }}
-        >
+          }}>
           <CloseIcon />
         </IconButton>
       ) : null}
     </DialogTitle>
   );
-}
+};
 
 BootstrapDialogTitle.propTypes = {
   children: PropTypes.node,
   onClose: PropTypes.func.isRequired,
 };
 
-const MiniCalendario = ({handleFecha, name}) => {
-
+const MiniCalendario = ({ handleFecha, name }) => {
   const initData = {
-    dia: 'Día',
+    dia: "Día",
     mes: "Mes",
-    numDia: ""
-  }
+    numDia: "",
+  };
   const [value, setValue] = useState(initData);
   const [open, setOpen] = React.useState(false);
 
@@ -67,50 +65,66 @@ const MiniCalendario = ({handleFecha, name}) => {
   };
 
   const handleDate = (newValue, name) => {
-    console.log(newValue)
+    console.log(newValue);
     const dia = newValue.$d.toString().split(" ")[0];
     const mes = newValue.$d.toString().split(" ")[1];
     const diaNum = newValue.$D;
-    setValue({dia: dia, mes: mes, diaNum: diaNum});
-    console.log(dia, mes, diaNum)
-    handleFecha({dia: dia, mes: mes, diaNum: diaNum}, name)
-  }
+    setValue({ dia: dia, mes: mes, diaNum: diaNum });
+    console.log(dia, mes, diaNum);
+    handleFecha({ dia: dia, mes: mes, diaNum: diaNum }, name);
+  };
 
   return (
     <div>
-      <div className='imagenCalendario' onClick={handleClickOpen}>
-      <div className='imgRoja' style={{display:'flex', justifyContent:'center',alignItems:'center'}}>
-        <Typography variant="subtitle2" sx={{color: 'white'}}>{value.mes}</Typography>
-      </div>
-      <div className='imgBlanca' style={{display:'flex', justifyContent:'center', flexDirection:'column', alignItems:'center'}}>
-        <Typography variant="subtitle2">{value.diaNum}</Typography>
-        <Typography variant="subtitle2">{value.dia}</Typography>
-      </div>
+      <div
+        className='imagenCalendario'
+        style={{ cursor: "pointer" }}
+        onClick={handleClickOpen}>
+        <div
+          className='imgRoja'
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
+          <Typography variant='subtitle2' sx={{ color: "white" }}>
+            {value.mes}
+          </Typography>
+        </div>
+        <div
+          className='imgBlanca'
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+          }}>
+          <Typography variant='subtitle2'>{value.diaNum}</Typography>
+          <Typography variant='subtitle2'>{value.dia}</Typography>
+        </div>
       </div>
       <div>
-      <BootstrapDialog
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
-          Calendario
-        </BootstrapDialogTitle>
-        <DialogContent dividers>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <StaticDatePicker 
-            value={value}
-            disablePast
-            onChange={(date) => handleDate(date, name)}
-            renderInput={(params) => <TextField {...params} />}
-            onClose={handleClose}
-            />
-          </LocalizationProvider>
-        </DialogContent>
-      </BootstrapDialog>
+        <BootstrapDialog aria-labelledby='customized-dialog-title' open={open}>
+          <BootstrapDialogTitle
+            id='customized-dialog-title'
+            onClose={handleClose}>
+            Calendario
+          </BootstrapDialogTitle>
+          <DialogContent dividers>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <StaticDatePicker
+                value={value}
+                disablePast
+                onChange={(date) => handleDate(date, name)}
+                renderInput={(params) => <TextField {...params} />}
+                onClose={handleClose}
+              />
+            </LocalizationProvider>
+          </DialogContent>
+        </BootstrapDialog>
+      </div>
     </div>
-    </div>
-  
   );
-}
+};
 
 export default MiniCalendario;
