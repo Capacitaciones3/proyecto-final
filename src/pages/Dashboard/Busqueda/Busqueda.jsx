3dfe7f2022a04f97bfe198f2969023b9f2a9f3bd
Capacitaciones3 <101343285+Capacitaciones3@users.noreply.github.com>
2023-04-14 18:37:44 -0300

    modificaciones en dashboard

--- src/pages/Dashboard/Busqueda/Busqueda.jsx
+++ src/pages/Dashboard/Busqueda/Busqueda.jsx
@@ -1,12 +1,17 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { TextField } from "@mui/material";
+import React from "react";
 // import SearchIcon from '@mui/icons-material/Search';
 
-const Busqueda = ({handleData}) => {
-
+const Busqueda = ({ handleData }) => {
   return (
-        <TextField id="outlined-search" label="Busqueda" type="search"  handleData={handleData}/>
-  )
-}
+    <TextField
+      id='outlined-search'
+      label='Busqueda'
+      type='search'
+      handleData={handleData}
+      fullWidth
+    />
+  );
+};
 
-export default Busqueda
+export default Busqueda;
--- src/pages/Dashboard/DashboardSecundario/DashboardSecundario.jsx
+++ src/pages/Dashboard/DashboardSecundario/DashboardSecundario.jsx
@@ -1,47 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import { Outlet } from 'react-router-dom';
-import CardUser from '../../../components/Cards/CardUser/CardUser'
-import CardApi from '../CardApi.jsx/CardApi'
-import { Typography } from '@mui/material';
-import Busqueda from '../Busqueda/Busqueda'
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import CardUser from "../../../components/Cards/CardUser/CardUser";
+import CardApi from "../CardApi.jsx/CardApi";
+import { Typography } from "@mui/material";
+import Busqueda from "../Busqueda/Busqueda";
 
-import './dashboardSecundario.css'
-import LicenciaCard from '../../../components/LicenciaCard/LicenciaCard';
+import "./dashboardSecundario.css";
+import LicenciaCard from "../../../components/LicenciaCard/LicenciaCard";
 
 const Dashboard = () => {
-
   const [data, setData] = useState({});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(data)
-  }
-
+    console.log(data);
+  };
 
   const handleData = (e) => {
     setData((old) => {
-      return ({
-        ...old, [e.target.name]: e.target.value  // para acceder a uno objeto dentro de un objeto []
-      })
-    })
-  }
+      return {
+        ...old,
+        [e.target.name]: e.target.value, // para acceder a uno objeto dentro de un objeto []
+      };
+    });
+  };
 
   useEffect(() => {
-    console.log(data)
-  }, [data])
-
+    console.log(data);
+  }, [data]);
 
   return (
     <>
-      <div style={{ width: '100vw', maxWidth: '100%', minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: '0px' }}>
-
-        <form style={{ width: '100%', display: 'flex', padding: '40px 40px 15px 40px', justifyContent: 'flex-start' }} onSubmit={handleSubmit}>
+      <div
+        style={{
+          width: "100vw",
+          maxWidth: "100%",
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          padding: "0px",
+        }}>
+        <form
+          style={{
+            width: "100%",
+            display: "flex",
+            padding: "40px 40px 15px 40px",
+            justifyContent: "flex-start",
+          }}
+          onSubmit={handleSubmit}>
           <div className='opciondeBusqueda'>
             <Busqueda />
           </div>
         </form>
 
-        <article style={{ width: '100%', display: 'flex', gap: '25px', padding: '15px 40px 40px 40px', justifyContent: 'space-between' }}>
+        <article
+          style={{
+            width: "100%",
+            display: "flex",
+            gap: "25px",
+            padding: "15px 40px 40px 40px",
+            justifyContent: "space-between",
+          }}>
           <section className='contenedorAPIs'>
             <div>
               <CardApi />
@@ -51,16 +71,28 @@ const Dashboard = () => {
             </div>
           </section>
           <section className='cardLicenciasPorAprobar2'>
-            <div className='titulo2'> <Typography variant="h6" color={'grey'}> Solicitudes pendientes: </Typography></div>
-            <div >
+            <div className='titulo2'>
+              {" "}
+              <Typography variant='h6' color={"grey"}>
+                {" "}
+                Solicitudes pendientes:{" "}
+              </Typography>
+            </div>
+            <div>
               <ul>
                 <CardUser />
               </ul>
             </div>
           </section>
           <section className='cardLicenciasProximas2'>
-            <div className='titulo2'> <Typography variant="h6" color={'grey'}> Proximas licencias aprobadas: </Typography></div>
-            <div >
+            <div className='titulo2'>
+              {" "}
+              <Typography variant='h6' color={"grey"}>
+                {" "}
+                Proximas licencias aprobadas:{" "}
+              </Typography>
+            </div>
+            <div>
               <ul>
                 <CardUser />
               </ul>
@@ -73,8 +105,7 @@ const Dashboard = () => {
         <LicenciaCard />
       </div>
     </>
-  )
-
-}
+  );
+};
 
 export default Dashboard;
