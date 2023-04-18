3409fe7f444371d58e1efa5c45ec28c6c48ef2a7
Capacitaciones3 <101343285+Capacitaciones3@users.noreply.github.com>
2023-04-18 12:26:32 -0300

    Delete dashboardServices.js
    merge con rama licencias

--- src/services/dashboardServices.js
+++
@@ -1,9 +0,0 @@
-import { fetchContent } from "../utils/fetch";
-
-export const getApi = async () => {
-  try {
-    return await fetchContent("/licencias");
-  } catch (error) {
-    throw new Error("[getApi service error]: " + error);
-  }
-};
