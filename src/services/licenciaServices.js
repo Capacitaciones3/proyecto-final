f1204ca9530a705133c956ec47765b3ae749d472
Capacitaciones3 <101343285+Capacitaciones3@users.noreply.github.com>
2023-04-21 10:08:02 -0300

    arreglé errores en licenciasServices

--- src/services/licenciaServices.js
+++ src/services/licenciaServices.js
@@ -2,7 +2,6 @@ import { fetchContent } from "../utils/fetch";
 
 export const getLicencias = async () => {
   try {
-    // const respuesta = await fetchContent("/api/licencias", { method: "GET" });
     const respuesta = await fetchContent("/licencias", { method: "GET" });
     return respuesta;
   } catch (error) {
@@ -12,9 +11,6 @@ export const getLicencias = async () => {
 
 export const getLicenciasPendientes = async (id) => {
   try {
-    const respuesta = await fetchContent(`/api/licencias/pendientes/${id}`, {
-      method: "GET",
-    });
     const respuesta = await fetchContent(`/api/licencias/pendientes/${id}`, {
       method: "GET",
     });
@@ -30,10 +26,6 @@ export const getLicenciasAprobadas = async (id) => {
       `/api/licencias/aprobadas/supervisor/${id}`,
       { method: "GET" }
     );
-    const respuesta = await fetchContent(
-      `/api/licencias/aprobadas/supervisor/${id}`,
-      { method: "GET" }
-    );
     return respuesta;
   } catch (error) {
     throw new Error("[getLicencias service error]: " + error);
@@ -45,24 +37,14 @@ export const getLicenciaFull = async (id) => {
     const respuesta = await fetchContent(`/api/licencias/full/${id}`, {
       method: "GET",
     });
-    const respuesta = await fetchContent(`/api/licencias/full/${id}`, {
-      method: "GET",
-    });
     return respuesta;
-  } catch (error) {
   } catch (error) {
     throw new Error("[getLicencias service error]: " + error);
   }
 };
-};
 
 export const actualizarDatosLicencias = async (idLicencia, estado) => {
   try {
-    const respuesta = await fetchContent(
-      `/api/licencias/update?idLicencia=${idLicencia}&estado=${estado}`,
-      { method: "PUT" }
-    );
-    return "La licencia a cambiado de estado";
     const respuesta = await fetchContent(
       `/api/licencias/update?idLicencia=${idLicencia}&estado=${estado}`,
       { method: "PUT" }
